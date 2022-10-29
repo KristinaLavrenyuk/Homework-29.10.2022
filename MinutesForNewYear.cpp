5bf28f8c8232a414b4ddbb4b0165e85df66256fc
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-10-29 14:07:08 +0300

    Add files via upload

---
+++ MinutesForNewYear.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t, m, h, mm, hh;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> h >> m;
+        mm = 60 - m;
+        hh = 23 - h;
+        hh *= 60;
+        cout << hh + mm << '\n';
+    }
+}
